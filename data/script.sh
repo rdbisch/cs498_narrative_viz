e15d03f7f84d8deb321ee217c85cffef9e4d31b1
Rick Bischoff <rdbisch@gmail.com>
2020-07-27 22:18:33 -0500

    I done goofed and added the CSV to lfs

--- data/script.sh
+++ data/script.sh
@@ -1,4 +1,4 @@
 cp lahmansbaseballdb.sqlite dev.sqlite
 ./sqlite3 dev.sqlite < preprocess.sql
-./sqlite3 -header -csv dev.sqlite "SELECT * FROM temp_t9;" > baseball_data.csv
+./sqlite3 -header -csv dev.sqlite "SELECT * FROM temp_t9;" > baseball_data.ccsv
 rm dev.sqlite
